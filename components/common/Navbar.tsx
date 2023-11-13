dd94add6d5bb16946f1a7ddb8c21a0e661ff18c9
rakibul-wdp <rakibul.wdp@gmail.com>
2023-11-13 10:16:14 +0600

    commit changes

---
+++ components/common/Navbar.tsx
@@ -0,0 +1,17 @@
+import Container from "./Container";
+
+const Navbar = () => {
+  return (
+    <Container>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li>About</li>
+        </ul>
+      </nav>
+    </Container>
+  );
+};
+
+export default Navbar;
