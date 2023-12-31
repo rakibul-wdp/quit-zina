f71c2500bf865857e972cf89e6c24ebd77cf17f1
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-31 14:53:21 +0600

    footer template

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,3 +1,45 @@
+import { Container } from "@/universal";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container>
+      <div className="mt-24 pb-3 flex justify-between">
+        <div>
+          <h3 className="text-lg text-primary font-bold mb-2">Quit Zina!</h3>
+          <ul>
+            <li>Level-4, 34, Awal Centre, Banani, Dhaka</li>
+            <li>Official: support@quitzina.com</li>
+            <li>Helpline : 01580530145 (Available : 10AM - 10PM)</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-lg font-bold mb-2">Useful Links</h3>
+          <ul>
+            <li>Refund policy</li>
+            <li>Terms and Conditions</li>
+            <li>Privacy Policy</li>
+            <li>App Privacy Policy</li>
+            <li>About us</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-lg font-bold mb-2">Follow Us</h3>
+          <ul className="flex items-center justify-between gap-2">
+            <li>TW/X</li>
+            <li>IN</li>
+            <li>YT</li>
+            <li>FB</li>
+          </ul>
+          <div className="mt-3">
+            <h4 className="text-lg font-medium">Download App</h4>
+            <p>Google Play</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="text-center py-5">
+        <p>Copyright &copy; {new Date().getFullYear()} Quit Zina</p>
+      </div>
+    </Container>
+  );
 };
