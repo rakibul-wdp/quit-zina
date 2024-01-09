aca900e6d4627924ced0705279789d0523ff5284
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-09 12:44:48 +0600

    create hook for stopwatch

--- components/Countdown/Stopwatch.tsx
+++ components/Countdown/Stopwatch.tsx
@@ -1,51 +1,51 @@
-"use client";
-
 import { Clock, Rank } from "@/components/Countdown";
+import useStopwatch from "@/hooks/useStopwatch";
 import { Container } from "@/universal";
-import { useState } from "react";
 
 export const Stopwatch = () => {
-  const [days, setDays] = useState(0);
-  const [hrs, setHrs] = useState(0);
-  const [mins, setMins] = useState(0);
-  const [secs, setSecs] = useState(0);
-  const [progressTime, setProgressTime] = useState(0);
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>();
+  const { days, hrs, mins, secs, progressTime, start, restart } =
+    useStopwatch();
+  // const [days, setDays] = useState(0);
+  // const [hrs, setHrs] = useState(0);
+  // const [mins, setMins] = useState(0);
+  // const [secs, setSecs] = useState(0);
+  // const [progressTime, setProgressTime] = useState(0);
+  // const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>();
 
-  let startTime = 0;
-  let elapsedTime = 0;
+  // let startTime = 0;
+  // let elapsedTime = 0;
 
-  const start = () => {
-    startTime = Date.now() - elapsedTime;
-    const id = setInterval(updateTime, 1000);
-    setIntervalId(id);
-  };
+  // const start = () => {
+  //   startTime = Date.now() - elapsedTime;
+  //   const id = setInterval(updateTime, 1000);
+  //   setIntervalId(id);
+  // };
 
-  const restart = () => {
-    clearInterval(intervalId);
-    setDays(0);
-    setHrs(0);
-    setMins(0);
-    setSecs(0);
-    startTime = 0;
-    elapsedTime = 0;
+  // const restart = () => {
+  //   clearInterval(intervalId);
+  //   setDays(0);
+  //   setHrs(0);
+  //   setMins(0);
+  //   setSecs(0);
+  //   startTime = 0;
+  //   elapsedTime = 0;
 
-    start();
-  };
+  //   start();
+  // };
 
-  const updateTime = () => {
-    elapsedTime = Date.now() - startTime;
-    const secs = Math.floor((elapsedTime / 1000) % 60);
-    const mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
-    const hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
-    const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
+  // const updateTime = () => {
+  //   elapsedTime = Date.now() - startTime;
+  //   const secs = Math.floor((elapsedTime / 1000) % 60);
+  //   const mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+  //   const hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+  //   const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
 
-    setDays(days);
-    setHrs(hrs);
-    setMins(mins);
-    setSecs(secs);
-    setProgressTime(elapsedTime);
-  };
+  //   setDays(days);
+  //   setHrs(hrs);
+  //   setMins(mins);
+  //   setSecs(secs);
+  //   setProgressTime(elapsedTime);
+  // };
 
   return (
     <Container>
