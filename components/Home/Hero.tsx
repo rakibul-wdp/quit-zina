c306feb4ab49e2a49a214fd7608825aa03c1a645
rakibul-wdp <rakibul.wdp@gmail.com>
2024-01-29 03:39:00 +0600

    home hero first part

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -10,12 +10,23 @@ export const Hero = () => {
             <h1 className="text-4xl font-bold mb-2">246 days</h1>
             <h3 className="text-xl font-medium">07:35:23</h3>
           </div>
-          <div>
+          <div className="pr-3">
             {homeRank.map((rank) => (
-              <div key={rank.id}>
-                <h1>{rank.rank}</h1>
-                <hr />
-                <p>{rank.reach}</p>
+              <div
+                className={`bg-white flex flex-col items-center justify-center px-10 py-2 rounded-lg ${
+                  rank.id !== 5 && "mb-3"
+                } ${rank.id === 3 && "border-2 border-green-500"}`}
+                key={rank.id}
+              >
+                <h1
+                  className={`text-xl font-medium ${
+                    rank.id === 3 && "text-green-500"
+                  }`}
+                >
+                  {rank.rank}
+                </h1>
+                <hr className="w-full my-[2px]" />
+                <p className="text-sm font-medium">{rank.reach}</p>
               </div>
             ))}
           </div>
