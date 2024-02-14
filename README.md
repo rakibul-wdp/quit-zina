be24f70dfe0e577ff88495cb3ab351726d8f59a3
rakibul-wdp <rakibul.wdp@gmail.com>
2024-02-14 14:21:09 +0600

    resume time when re load

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Quit Zina!
+
+It will help people, by quitting addiction how to be a good muslim. It's mainly focus, how to quit p\*\*n addiction, that's call quit all of zina (thinking in mind, watching p\*\*n & soft p\*\*n, doing m\*\*n).
+
+## Motivation
+
+### Goal
+
+Quick Start
+Usage
+Contributing
--- hooks/useStopwatch.ts
+++ hooks/useStopwatch.ts
@@ -1,5 +1,71 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+export default function useStopwatch() {
+  const [days, setDays] = useState(0);
+  const [hrs, setHrs] = useState(0);
+  const [mins, setMins] = useState(0);
+  const [secs, setSecs] = useState(0);
+  const [progressTime, setProgressTime] = useState(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>();
+
+  useEffect(() => {
+    const resumeTime = JSON.parse(localStorage.getItem("resumeTime") ?? "null");
+    if (resumeTime) {
+      const elapsed = Date.now() - resumeTime;
+      const id = setInterval(updateTime, 1000);
+      setProgressTime(elapsed);
+      setIntervalId(id);
+    }
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const start = () => {
+    const startTime = Date.now() - progressTime;
+    localStorage.setItem("resumeTime", JSON.stringify(startTime));
+
+    const id = setInterval(updateTime, 1000);
+    setIntervalId(id);
+  };
+
+  const restart = () => {
+    clearInterval(intervalId);
+    setDays(0);
+    setHrs(0);
+    setMins(0);
+    setSecs(0);
+
+    localStorage.removeItem("resumeTime");
+    setProgressTime(0);
+
+    start();
+  };
+
+  const updateTime = () => {
+    const elapsedTime =
+      Date.now() - JSON.parse(localStorage.getItem("resumeTime") ?? "null");
+
+    const secs = Math.floor((elapsedTime / 1000) % 60);
+    const mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    const hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
+    const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
+
+    setDays(days);
+    setHrs(hrs);
+    setMins(mins);
+    setSecs(secs);
+    setProgressTime(elapsedTime);
+  };
+
+  return { days, hrs, mins, secs, progressTime, start, restart };
+}
+
+/*
+"use client";
+
 import { useState } from "react";
 
 export default function useStopwatch() {
@@ -50,3 +116,5 @@ export default function useStopwatch() {
 
   return { days, hrs, mins, secs, progressTime, start, restart };
 }
+
+*/
