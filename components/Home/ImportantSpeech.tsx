f47a759b558ac34a02ee45c18626b5394e69e3d6
rakibul-wdp <rakibul.wdp@gmail.com>
2024-04-12 12:06:20 +0600

    changes

--- components/Home/ImportantSpeech.tsx
+++ components/Home/ImportantSpeech.tsx
@@ -6,11 +6,17 @@ export const ImportantSpeech = () => {
     <Container>
       <div className="mt-24 flex items-start justify-between gap-10">
         {importantSpeeches.map((speech) => (
-          <div className="basis-1/3" key={speech.id}>
+          <div
+            className="basis-1/3 border border-primary rounded p-4 h-[480px]"
+            key={speech.id}
+          >
             <h2 className="text-2xl font-semibold mb-3 text-center">
               {speech.title}
             </h2>
-            <p className="text-justify">{speech.details}</p>
+            <p className="text-justify">
+              {/* {speech.details} */}
+              {speech.details.replace(/<br\s*\/?>/g, "${<br>}")}
+            </p>
           </div>
         ))}
       </div>
