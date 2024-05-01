5747c4df3833565f20dc31f2fd24aeb619fde2b7
rakibul-wdp <rakibul.wdp@gmail.com>
2024-05-01 15:36:53 +0600

    gives style in important speech

--- components/Home/ImportantSpeech.tsx
+++ components/Home/ImportantSpeech.tsx
@@ -7,15 +7,26 @@ export const ImportantSpeech = () => {
       <div className="mt-24 flex items-start justify-between gap-10">
         {importantSpeeches.map((speech) => (
           <div
-            className="basis-1/3 border border-primary rounded p-4 h-[480px]"
+            // className="basis-1/3 border border-primary rounded p-4 h-[520px]"
+            className="basis-1/3 p-4"
             key={speech.id}
           >
             <h2 className="text-2xl font-semibold mb-3 text-center">
               {speech.title}
             </h2>
             <p className="text-justify">
-              {/* {speech.details} */}
-              {speech.details.replace(/<br\s*\/?>/g, "${<br>}")}
+              {speech.id === 1 && speech.details}
+              {speech.id === 2 && speech.details.slice(0, 405)}
+              {speech.id === 3 && speech.details.slice(0, 161)}
+            </p>
+            <br />
+            <p className="text-justify">
+              {speech.id === 2 && speech.details.slice(412)}
+              {speech.id === 3 && speech.details.slice(168, 505)}
+            </p>
+            <br />
+            <p className="text-justify">
+              {speech.id === 3 && speech.details.slice(512)}
             </p>
           </div>
         ))}
