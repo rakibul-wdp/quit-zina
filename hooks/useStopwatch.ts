143cab88de187d56fd44eec17c701123b282cb9c
rakibul-wdp <rakibul.wdp@gmail.com>
2025-01-01 16:27:16 +0600

    get hints from vclock

--- hooks/useStopwatch.ts
+++ hooks/useStopwatch.ts
@@ -64,57 +64,11 @@ export default function useStopwatch() {
 }
 
 /*
-"use client";
+on vclcok:
+when running: start=2025-01-01T15:33:57.547  (it also in url)
+when stop: enabled=0&msec=27906 (it also in url)
 
-import { useState } from "react";
-
-export default function useStopwatch() {
-  const [days, setDays] = useState(0);
-  const [hrs, setHrs] = useState(0);
-  const [mins, setMins] = useState(0);
-  const [secs, setSecs] = useState(0);
-  const [progressTime, setProgressTime] = useState(0);
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>();
-
-  const start = () => {
-    const elapsedTime = JSON.parse(
-      localStorage.getItem("elapsedTime") ?? "null"
-    );
-    localStorage.setItem("startTime", `${Date.now() - elapsedTime}`);
-
-    const id = setInterval(updateTime, 1000);
-    setIntervalId(id);
-  };
-
-  const restart = () => {
-    clearInterval(intervalId);
-    setDays(0);
-    setHrs(0);
-    setMins(0);
-    setSecs(0);
-
-    localStorage.setItem("elapsedTime", "0");
-    start();
-  };
-
-  const updateTime = () => {
-    const startTime = JSON.parse(localStorage.getItem("startTime") ?? "null");
-    const elapsedTime = Date.now() - startTime;
-    localStorage.setItem("elapsedTime", `${elapsedTime}`);
-
-    const secs = Math.floor((elapsedTime / 1000) % 60);
-    const mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
-    const hrs = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
-    const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24));
-
-    setDays(days);
-    setHrs(hrs);
-    setMins(mins);
-    setSecs(secs);
-    setProgressTime(elapsedTime);
-  };
-
-  return { days, hrs, mins, secs, progressTime, start, restart };
-}
+Start time: Current time
+Stop time: Counted
 
 */
